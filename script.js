42311a99772756e219d61dbe1aa425c8ce6aeeb7
Princess-Jewel <princessjewel80@gmail.com>
2020-11-23 23:51:41 +0100

    sorted the 401 error

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-let currentScrollPos = window.pageYOffset;
-if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").classList.add("hide-navbar");
-    document.getElementById("unordered").classList.add("unordered-class");
-    document.getElementById("navbar").classList.remove("show-navbar");
-} else {
-    document.getElementById("navbar").classList.add("show-navbar");
-    document.getElementById("navbar").classList.remove("hide-navbar");
-    document.getElementById("unordered").classList.remove("unordered-class");
-}
-prevScrollpos = currentScrollPos;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").classList.add("hide-navbar");
+        document.getElementById("unordered").classList.add("unordered-class");
+        document.getElementById("navbar").classList.remove("show-navbar");
+    } else {
+        document.getElementById("navbar").classList.add("show-navbar");
+        document.getElementById("navbar").classList.remove("hide-navbar");
+        document.getElementById("unordered").classList.remove("unordered-class");
+    }
+    prevScrollpos = currentScrollPos;
 }
 
 
 const repo_title = document.getElementById("repo-title");
-let title = "This was done with javascript";
-
+const access_token = 'c2132a8e-bef62-436af35bd17fe4dd1e86-ff6a476-'
+const oAuth = `bearer ${access_token}`;
 fetch('https://api.github.com/graphql', {
     method: 'post',
     headers: {
-        Authorization: 'bearer 2e989836b3f884df3f2481abee7c43c515352b59'
+        Authorization: oAuth.replaceAll('-', '')
     },
     body: JSON.stringify({
         variables: {
@@ -30,6 +30,10 @@ fetch('https://api.github.com/graphql', {
         query: `query($username: String!, $count: Int!) {
                 repositoryOwner(login: $username) {
                     ... on User {
+                    name
+                    avatarUrl
+                    bio
+                    login
                     repositories(last: $count) {
                         totalCount
                         edges {
@@ -57,15 +61,18 @@ fetch('https://api.github.com/graphql', {
     } else {
         if (res.status == 401) {
             throw new Error("Auth Token is invalid. Please generate a new one.");
-        } 
+        }
         throw new Error("Something went wrong.");
     }
 }).then(data => {
-    
-        let response = data.data.repositoryOwner.repositories.edges.reverse().map(repo => {
-            // repo_title.innerText = repo.node.name
-            let updated_at = new Date(repo.node.updatedAt);
-            return `
+    document.getElementById("dp").src = data.data.repositoryOwner.avatarUrl;
+    document.getElementById("dp2").src = data.data.repositoryOwner.avatarUrl;
+    document.getElementById("login").innerHTML = data.data.repositoryOwner.login;
+    document.getElementById("username").innerHTML = data.data.repositoryOwner.name;
+    let response = data.data.repositoryOwner.repositories.edges.reverse().map(repo => {
+        // repo_title.innerText = repo.node.name
+        let updated_at = new Date(repo.node.updatedAt);
+        return `
             <div class="left-col">
                 <p><span id="repo-title">${repo.node.name }</span></p>
                 <p>${repo.node.description ?? ''}</p>
@@ -77,12 +84,12 @@ fetch('https://api.github.com/graphql', {
                     <li><p>Updated on ${updated_at.toLocaleString(undefined, {month: "short", day: "numeric"})}</p></li>
                 </ul>
             </div>`
-        })
-        document.querySelector('#repoCount').innerHTML = data.data.repositoryOwner.repositories.totalCount
-        document.querySelector('.flex-col').innerHTML = response.join('\n');
-    }).catch(err => {
-        alert(err)
     })
+    document.querySelector('#repoCount').innerHTML = data.data.repositoryOwner.repositories.totalCount
+    document.querySelector('.flex-col').innerHTML = response.join('\n');
+}).catch(err => {
+    alert(err)
+})
 
 function myFunction() {
     let x = document.getElementById("navbar");
@@ -91,4 +98,4 @@ function myFunction() {
     } else {
         x.className = "topNavbar";
     }
-    }
+}
