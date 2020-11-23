bd645adf9e3915995aaccf081d64cda791eb7847
Princess-Jewel <princessjewel80@gmail.com>
2020-11-23 02:50:41 +0100

    i added the media queries

---
+++ script.js
@@ -0,0 +1,84 @@
+let prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").classList.add("hide-navbar");
+    document.getElementById("unordered").classList.add("unordered-class");
+    document.getElementById("navbar").classList.remove("show-navbar");
+} else {
+    document.getElementById("navbar").classList.add("show-navbar");
+    document.getElementById("navbar").classList.remove("hide-navbar");
+    document.getElementById("unordered").classList.remove("unordered-class");
+}
+prevScrollpos = currentScrollPos;
+}
+
+
+const repo_title = document.getElementById("repo-title");
+let title = "This was done with javascript";
+// const oauth = {Authorization: 'bearer ' + b912b08f4c81196d59a736c82f16b062baeb37bc}
+// repo_title.innerText = title
+fetch('https://api.github.com/graphql', {
+    method: 'post',
+    headers: {
+        Authorization: 'bearer cf4a9f9139146d5d7b24e5fbd3e143a6008d1f4b'
+    },
+    body: JSON.stringify({
+        variables: {
+            "username": "princess-jewel",
+            "count": 20
+        },
+        query: `query($username: String!, $count: Int!) {
+                repositoryOwner(login: $username) {
+                    ... on User {
+                    repositories(last: $count) {
+                        totalCount
+                        edges {
+                        node {
+                            name
+                            description
+                            url
+                            stargazerCount
+                            primaryLanguage {
+                            name
+                            color
+                            }
+                            forkCount
+                            updatedAt
+                        }
+                        }
+                    }
+                    }
+                }
+            }`
+    })
+}).then(res => res.json()).then(data => {
+    
+        let response = data.data.repositoryOwner.repositories.edges.reverse().map(repo => {
+            // repo_title.innerText = repo.node.name
+            let updated_at = new Date(repo.node.updatedAt);
+            return `
+            <div class="left-col">
+                <p><span id="repo-title">${repo.node.name }</span></p>
+                <p>${repo.node.description ?? ''}</p>
+                <i class="far fa-star star">Star</i>
+                <ul>
+                    <li style"display: ${repo.node.primaryLanguage == null ? "none;" : "block"}><i class="fas fa-circle" style="color: ${repo.node.primaryLanguage == null ? "#fff; display: none;" : repo.node.primaryLanguage.color}"></i>${repo.node.primaryLanguage == null ? "" : repo.node.primaryLanguage.name}</li>
+                    <li><i class="far fa-star"></i>${repo.node.stargazerCount}</li>
+                    <li><i class="fas fa-code-branch"></i>${repo.node.forkCount}</li>
+                    <li><p>Updated on ${updated_at.toLocaleString(undefined, {month: "short", day: "numeric"})}</p></li>
+                </ul>
+            </div>`
+        })
+        document.querySelector('#repoCount').innerHTML = data.data.repositoryOwner.repositories.totalCount
+        document.querySelector('.flex-col').innerHTML = response.join('\n');
+    })
+
+function myFunction() {
+    let x = document.getElementById("navbar");
+    if (x.className === "topNavbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "topNavbar";
+    }
+    }
